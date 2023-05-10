23f48db8a087fa4f0fcdae59f7106c86c8243d6c
Yaroslav Dyagilev <yaroslav0dyagilev@gmail.com>
2023-05-10 18:00:09 +0300

    Фиксация

--- users/forms.py
+++ users/forms.py
@@ -7,12 +7,12 @@ def validete_username(value):
         raise forms.ValidationError(f"Your username cannot be of {len(value)}  word")
 
 class CreateUser(UserCreationForm):
-    password1 = forms.CharField(label="Password", widget = forms.PasswordInput(attrs={"placeholder":"Password",'autocomplete':'new-password','class':'form-control'}),error_messages={"required":"Please enter password"},)
-    password2 = forms.CharField(label="Re-enter",widget= forms.PasswordInput(attrs={"placeholder":"Re-Enter",'autocomplete':'new-password','class':'form-control'}),help_text="Make sure your password contains 'small letter','capital letter','numbers' and 'symbols'",error_messages={"required":"Re-Enter password field cannot be empty"})
-    username = forms.CharField(label="username",widget=forms.TextInput(attrs={"placeholder":"Username","id":"username",'class':'form-control'}),validators=[validete_username])
-    first_name = forms.CharField(widget=forms.TextInput(attrs={"placeholder":"First Name","required":True,'class':'form-control'}),error_messages={"required":"First name cannot be empty"})
-    last_name = forms.CharField(widget=forms.TextInput(attrs={"placeholder":"First Name","required":True,'class':'form-control'}),error_messages={"required":"Last name cannot be empty"})
-    email = forms.CharField(widget=forms.EmailInput(attrs={"required":True,"Placeholder":"Email",'class':'form-control'}),error_messages={'required':'Email fields should not be empty'})
+    password1 = forms.CharField(label="Пароль", widget = forms.PasswordInput(attrs={"placeholder":"Пароль",'autocomplete':'new-password','class':'form-control'}),error_messages={"required":"Please enter password"},)
+    password2 = forms.CharField(label="Введіть пароль ще раз",widget= forms.PasswordInput(attrs={"placeholder":"Введіть пароль ще раз",'autocomplete':'new-password','class':'form-control'}),help_text="Переконайтеся, що ваш пароль містить малі літери, великі літери, цифри та символи",error_messages={"required":"Re-Enter password field cannot be empty"})
+    username = forms.CharField(label="Ім'я користувача",widget=forms.TextInput(attrs={"placeholder":"Ім'я користувача","id":"username",'class':'form-control'}),validators=[validete_username])
+    first_name = forms.CharField(label="Ім'я",widget=forms.TextInput(attrs={"placeholder":"Ім'я","required":True,'class':'form-control'}),error_messages={"required":"First name cannot be empty"})
+    last_name = forms.CharField(label="Прізвище",widget=forms.TextInput(attrs={"placeholder":"Прізвище","required":True,'class':'form-control'}),error_messages={"required":"Last name cannot be empty"})
+    email = forms.CharField(label="Пошта",widget=forms.EmailInput(attrs={"required":True,"Placeholder":"Пошта",'class':'form-control'}),error_messages={'required':'Email fields should not be empty'})
     class  Meta:
         model = User
         fields =['username','first_name','last_name','email','password1','password2']
@@ -23,5 +23,5 @@ class VerifyForm(forms.Form):
 
 
 class LoginForm(AuthenticationForm):
-    username = UsernameField(widget=forms.TextInput(attrs={"placeholder":"Username","class":"form-control"}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"password",'autocomplete':'current-password',"class":"form-control"}))  
+    username = UsernameField(label="Ім'я користувача",widget=forms.TextInput(attrs={"placeholder":"Ім'я користувача","class":"form-control"}))
+    password = forms.CharField(label="Пароль",widget=forms.PasswordInput(attrs={"placeholder":"Пароль",'autocomplete':'current-password',"class":"form-control"}))  
